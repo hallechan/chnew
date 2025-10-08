721c9d13e5ae8d2222915e69a29dcaa40f377705
Halle Chan <hallechanne@gmail.com>
2025-10-07 21:31:21 -0400

    medisyn description + link

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,6 +8,25 @@ function Projects() {
               <span className="relative z-10 text-blackish">roject</span>
             </span>s!
           </h3>
+          <a href="https://devpost.com/software/medisyn" target="_blank" rel="noreferrer">
+            <div className="flex flex-col md:flex-row relative mt-10">
+              <div
+                className="p-6 w-2/3 z-10 bg-no-repeat bg-cover"
+                style={{ backgroundImage: 'url("/assets/decor/smallpaper.png")' }}
+              >
+                <div className="flex-col justify-between p-6">
+                  <h4>building a medical assessment tool to eliminate diagnostic biases & sex-based discrimination.</h4>
+                  <div><br/>
+                    <img className="w-1/2" src="/assets/logos/medisyntext.png" />
+                    <h4>technova '25 1st place overall</h4>
+                  </div>
+                </div>
+              </div>
+              <div className="md:w-1/2 flex justify-end -mt-10 md:-mt-0 md:-ml-20 z-20">
+                <img src="/assets/photos/medisyn.png" />
+              </div>
+            </div>
+          </a>
           <a href="https://devpost.com/software/pitta-patta" target="_blank" rel="noreferrer">
             <div className="flex flex-col md:flex-row relative mt-10">
               <div
